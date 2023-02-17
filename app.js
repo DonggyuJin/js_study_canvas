188fbfb57286c0bcea087666699a216ab94a419f
jdg70 <jdk2531@gmail.com>
2023-02-17 20:57:21 +0900

    add fillRect, arc, beginPath

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(255 - 40, 200 - 30, 60, 100);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270, 80, 8, 1 * Math.PI, 2 * Math.PI);
+ctx.arc(230, 80, 8, 1 * Math.PI, 2 * Math.PI);
+ctx.fill();
